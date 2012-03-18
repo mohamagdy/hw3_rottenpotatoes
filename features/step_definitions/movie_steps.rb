a8f2a16a40116ad07812829ca5ba7779de5b25de
Mohamed Magdy <mohamed.magdy@badrit.com>
2012-03-19 01:07:01 +0200

    Part 1 added

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,20 +1,19 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+  Movie.delete_all
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(movie)
   end
-  assert false, "Unimplmemented"
 end
-
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  assert false, "Unimplmemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
